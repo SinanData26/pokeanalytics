52c7c22048dc0f2ead255b33c0c80c4e30f025df
SinanData26 <sinanozcetin@gmail.com>
2026-04-29 23:46:40 +0200

    exploring api data

---
+++ src/explorepokemon.py
@@ -0,0 +1,22 @@
+import requests
+import json
+from config import POKEAPI_BASE_URL
+
+url = "https://pokeapi.co/api/v2/pokemon?limit=10"
+
+response = requests.get(url)
+
+print(response.status_code)
+
+data = response.json()
+
+# print(json.dumps(data, indent=2))
+
+pokemon_list = data["results"]
+
+for p in pokemon_list:
+    detail_url = p["url"]
+    detail_response = requests.get(detail_url)
+    detail_data = detail_response.json()
+    
+    print(detail_data["name"])
---
+++ src/writejsonlist.py
@@ -0,0 +1,12 @@
+import requests
+import json
+from config import POKEAPI_BASE_URL
+
+url = POKEAPI_BASE_URL
+
+response = requests.get(url)
+
+data = response.json()
+
+with open("resourcelist.json", "w") as f:
+    json.dump(data, f, indent=2)
